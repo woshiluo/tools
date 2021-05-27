3d000c4b6679a2e7c6d115a8077830f394210d3a
Woshiluo Luo <woshiluo.luo@outlook.com>
2021-05-27 09:45:02 +0800

    feat(usbreload/src/main.rs): use bind

--- usbreload/src/main.rs
+++ usbreload/src/main.rs
@@ -1,23 +1,49 @@
 extern crate pretty_env_logger;
 
+enum Status {
+    Bind,
+    Unbind,
+}
+
+fn set_device(device: rusb::Device<rusb::GlobalContext>, status: Status) -> std::io::Result<()> {
+    let bus_number = device.bus_number();
+    let address = device.address();
+    let device_name = format!("{:03}-{:03}", bus_number, address);
+    log::info!(
+        "{} {}",
+        match status {
+            Status::Bind => "binding",
+            Status::Unbind => "Unbinding",
+        },
+        device_name
+    );
+
+    std::fs::write(
+        match status {
+            Status::Bind => "/sys/bus/usb/drivers/usb/bind",
+            Status::Unbind => "/sys/bus/usb/drivers/usb/unbind",
+        },
+        format!("{}-{}", bus_number, address),
+    )?;
+
+    Ok(())
+}
+
 fn main() {
     pretty_env_logger::init();
     for device in rusb::devices().unwrap().iter() {
-        let device_name = format!("{:03}-{:03}", device.bus_number(), device.address());
-        log::info!("Reseting {}", device_name);
-        let mut handle = match device.open() {
-            Ok(content) => content,
-            Err(err) => {
-                log::error!("Failed to open {}: {}", device_name, err);
-                continue;
-            }
-        };
-        match handle.reset() {
-            Ok(content) => content,
-            Err(err) => {
-                log::error!("Failed to reset {}: {}", device_name, err);
-                continue;
-            }
-        };
+        match set_device(device, Status::Unbind) {
+            Err(err) => log::error!("{}", err),
+            _ => (),
+        }
+    }
+
+    std::thread::sleep(std::time::Duration::from_secs(1));
+
+    for device in rusb::devices().unwrap().iter() {
+        match set_device(device, Status::Bind) {
+            Err(err) => log::error!("{}", err),
+            _ => (),
+        }
     }
 }
